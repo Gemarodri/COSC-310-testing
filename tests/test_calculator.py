31bcde9fda7317dd29c7e9bb604be3554471c856
Gema Rodriguez Perez <5637220+Gemarodri@users.noreply.github.com>
2025-10-20 11:06:50 -0700

    Adding add functionality

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -9,6 +9,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
+        self.assertEqual(add(1.5, 5.6), 7.1)
 
 if __name__ == "__main__":
     unittest.main()
